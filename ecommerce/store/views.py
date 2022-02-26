4efbcadbc8200970bec405ab0af6c71a84a7c085
sasanpy <sasan.sp92@hotmail.com>
2022-02-27 00:01:59 +0330

    Display product in store

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+from store.models import Product
+
+
+def store(request):
+    products = Product.objects.all().filter(is_available=True)
+    product_count = products.count()
+    context = {
+        'products': products,
+        'product_count': product_count
+    }
+    return render(request, 'store/store.html', context)
