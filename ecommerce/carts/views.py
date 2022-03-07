f742a08f9ea6ec1270b72b33d4e118f7c0c8f364
sasanpy <sasan.sp92@hotmail.com>
2022-03-08 00:45:23 +0330

    Cart add and remove.

--- ecommerce/carts/views.py
+++ ecommerce/carts/views.py
@@ -1,5 +1,58 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from carts.models import Cart, CartItem
+from store.models import Product
 
 
-def cart(requests):
-    return render(requests, 'store/cart.html')
+def _cart_id(request):
+    cart = request.session.session_key
+    if not cart:
+        cart = request.session.create()
+    return cart
+
+
+def add_cart(request, product_id):
+    product = Product.objects.get(id=product_id)
+    try:
+        cart = Cart.objects.get(cart_id=_cart_id(request))
+    except Cart.DoesNotExist:
+        cart = Cart.objects.create(
+            cart_id=_cart_id(request)
+        )
+    cart.save()
+
+    try:
+        cart_item = CartItem.objects.get(product=product, cart=cart)
+        cart_item.quantity += 1
+        cart_item.save()
+    except CartItem.DoesNotExist:
+        cart_item = CartItem.objects.create(
+            product=product,
+            quantity=1,
+            cart=cart
+        )
+    cart_item.save()
+    return redirect('cart')
+
+
+def cart(requests, total=0, quantity=0, is_active=None):
+    try:
+        cart = Cart.objects.get(cart_id=_cart_id(requests))
+        cart_items = CartItem.objects.filter(cart=cart, is_active=True)
+        for cart_item in cart_items:
+            total += (cart_item.product.price * cart_item.quantity)
+            quantity = cart_item.quantity
+        tax = (2 * total) / 100
+        grand_total = total + tax
+    except Cart.DoesNotExist or CartItem.DoesNotExist:
+        pass
+
+    context = {
+        'total': total,
+        'quantity': quantity,
+        'cart_items': cart_items,
+        'tax': tax,
+        'grand_total': grand_total
+    }
+
+    return render(requests, 'store/cart.html', context)
