38eeb6d7ad5378f31f59b8221e25745596d6dff1
sasanpy <sasan.sp92@hotmail.com>
2022-03-10 13:29:33 +0330

    Paginator

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
 
 from store.models import Product
@@ -14,13 +15,18 @@ def store(request, category_slug=None):
     if category_slug is not None:
         categories = get_object_or_404(Category, slug=category_slug)
         products = Product.objects.filter(category=categories, is_available=True)
+        paginator = Paginator(products, 1)
+        page = request.GET.get('page')
+        paged_products = paginator.get_page(page)
         product_count = products.count()
-
     else:
         products = Product.objects.filter(is_available=True)
+        paginator = Paginator(products, 6)
+        page = request.GET.get('page')
+        paged_products = paginator.get_page(page)
         product_count = products.count()
     context = {
-        'products': products,
+        'products': paged_products,
         'product_count': product_count
     }
     return render(request, 'store/store.html', context)
