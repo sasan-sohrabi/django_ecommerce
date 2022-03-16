194b7b6f9541435e659443df3df49ec1cf3080ab
sasanpy <sasan.sp92@hotmail.com>
2022-03-16 09:43:18 +0330

    Complete variation for project.

--- ecommerce/carts/urls.py
+++ ecommerce/carts/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.cart, name='cart'),
     path('add_cart/<int:product_id>/', views.add_cart, name='add_cart'),
-    path('decrement_cart/<int:product_id>/', views.decrement_cart_item, name='decrement_cart'),
-    path('remove_cart_item/<int:product_id>/', views.remove_cart_item, name='remove_cart_item'),
+    path('decrement_cart/<int:product_id>/<int:cart_item_id>/', views.decrement_cart_item, name='decrement_cart'),
+    path('remove_cart_item/<int:product_id>/<int:cart_item_id>/', views.remove_cart_item, name='remove_cart_item'),
 
 ]
